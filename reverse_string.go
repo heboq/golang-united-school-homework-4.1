be0924da7bd000033424c9e35d6fcf2733321cbc
Mark <61473070+heboq@users.noreply.github.com>
2022-06-12 00:55:11 +0700

    add a more efficient solution

--- reverse_string.go
+++ reverse_string.go
@@ -1,9 +1,12 @@
 package reverse_string
 
-func ReverseString(input string) (output string) {
+import "strings"
+
+func ReverseString(input string) string {
+	var output strings.Builder
 	runes := []rune(input)
 	for i := len(runes) - 1; i >= 0; i-- {
-		output += string(runes[i])
+		output.WriteRune(runes[i])
 	}
-	return output
+	return output.String()
 }
